cde3422d62e979f3f828cabc3f254e1a7f231014
rehamsaad <rehamsd2@gmail.com>
2025-12-29 22:41:50 +0200

    Initial Commit

---
+++ FlowerMarket(SwiftUI-MVVM)/FlowerMarket_SwiftUI_MVVM_App.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerMarket_SwiftUI_MVVM_App.swift
+//  FlowerMarket(SwiftUI-MVVM)
+//
+//  Created by Reham on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowerMarket_SwiftUI_MVVM_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
